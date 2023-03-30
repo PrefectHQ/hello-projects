a3f2acffe2d5d95500b07689160632cda9c59608
Chris White <chris@prefect.io>
2023-03-30 11:54:26 -0700

    Also log some config

--- flows/log_flow.py
+++ flows/log_flow.py
@@ -1,3 +1,4 @@
+import json
 import sys
 from utilities import AN_IMPORTED_MESSAGE
 
@@ -10,12 +11,23 @@ def log_task(name):
     logger = get_run_logger()
     logger.info("Hello %s!", name)
     logger.info("Prefect Version = %s 🚀", prefect.__version__)
-    logger.debug(AN_IMPORTED_MESSAGE)
+    logger.info(AN_IMPORTED_MESSAGE)
+
+
+@task
+def log_config():
+    logger = get_run_logger()
+
+    with open("config.json") as f:
+        config = json.load(f)
+
+    logger.info("Found config %s", config)
 
 
 @flow
 def log_flow(name: str = "Marvin"):
     log_task(name)
+    log_config()
 
 
 if __name__ == "__main__":
